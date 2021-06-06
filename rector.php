c4306b023495c61875aa533c6338fee066a662dd
zingimmick <zingimmick@outlook.com>
2021-06-06 17:05:35 +0800

    Rename file to respect class name

--- rector.php
+++ rector.php
@@ -4,11 +4,13 @@ declare(strict_types=1);
 
 use Rector\Core\Configuration\Option;
 use Rector\PSR4\Rector\Namespace_\MultipleClassFileToPsr4ClassesRector;
+use Rector\Restoration\Rector\ClassLike\UpdateFileNameByClassNameFileSystemRector;
 use Symfony\Component\DependencyInjection\Loader\Configurator\ContainerConfigurator;
 
 return static function (ContainerConfigurator $containerConfigurator): void {
     $services = $containerConfigurator->services();
     $services->set(MultipleClassFileToPsr4ClassesRector::class);
+    $services->set(UpdateFileNameByClassNameFileSystemRector::class);
 
     $parameters = $containerConfigurator->parameters();
     $parameters->set(Option::PATHS, [
