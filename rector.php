ce4152e7471a5b7d1f434aabcf10afe133eac810
zingimmick <zingimmick@outlook.com>
2022-05-23 23:27:03 +0800

    Add support for new ECSConfig and RectorConfig class

--- rector.php
+++ rector.php
@@ -7,13 +7,10 @@ use Rector\PSR4\Rector\Namespace_\MultipleClassFileToPsr4ClassesRector;
 use Rector\Restoration\Rector\ClassLike\UpdateFileNameByClassNameFileSystemRector;
 use Symfony\Component\DependencyInjection\Loader\Configurator\ContainerConfigurator;
 
-return static function (ContainerConfigurator $containerConfigurator): void {
-    $services = $containerConfigurator->services();
-    $services->set(MultipleClassFileToPsr4ClassesRector::class);
-    $services->set(UpdateFileNameByClassNameFileSystemRector::class);
-
-    $parameters = $containerConfigurator->parameters();
-    $parameters->set(Option::PATHS, [
+return static function (\Rector\Config\RectorConfig $rectorConfig): void {
+    $rectorConfig->rule(MultipleClassFileToPsr4ClassesRector::class);
+    $rectorConfig->rule(UpdateFileNameByClassNameFileSystemRector::class);
+    $rectorConfig->paths([
         __DIR__ . '/OpenSearch',
     ]);
 };
